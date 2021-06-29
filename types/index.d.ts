40a5b3c55492b018facd2ffbaa75784511a526cb
Dmitriy Pushkov <ezze@ezze.org>
2021-06-29 16:33:19 +0300

    ESLint rules for interface semicolon checkings are added.

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,27 +5,27 @@ declare type EventParamTransformer = (param: any) => any;
 
 declare type ErrorCallback = (error: Error | string) => void;
 
-declare type EventBusOptions = {
+declare interface EventBusOptions {
   paramTransformer?: EventParamTransformer;
   errorCallback?: ErrorCallback;
 }
 
-declare type EventHandlerExecution = {
+declare interface EventHandlerExecution {
   name: string;
   params: Array<any>;
   handler: EventHandler;
   hash: string;
-};
+}
 declare type EventHandlerExecutions = Array<EventHandlerExecution>;
 
 declare type EventHashes = Array<string>;
 
-declare type EventFireOptions = {
+declare interface EventFireOptions {
   throwError?: boolean;
   skip?: EventHashes;
 }
 
-declare type EventFireResult = {
+declare interface EventFireResult {
   done: EventHandlerExecutions;
   skipped: EventHandlerExecutions;
 }
